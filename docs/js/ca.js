4f6a75a45e2cea762b6b8415448f55c73415b1af
Enrique Iborra <47239270+EnriqueIborra@users.noreply.github.com>
2026-07-13 09:18:35 +0200

    Add files via upload

--- docs/js/ca.js
+++ docs/js/ca.js
@@ -1003,7 +1003,7 @@ document.querySelectorAll(".zoomable").forEach(img => {
 
 
 /*==========================================
-    Posar el mateix footer a totes les ca_xy.html    // &#x1F16D; &#x1F16F; &#x1F10E; &#x1F10F;  
+    Posar el mateix footer a totes les ca_xy.html    
 ==========================================*/
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -1016,12 +1016,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         <p>Criteris d'avaluació</p>
 
-         <p style="font-family: 'CCSymbols'; font-size:larger;" >
-         2026 ·· Llicència:  
-         
-         <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a><img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"><img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"><img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;">
-         
-         </p>
+    <p style="font-family:'CCSymbols',font-size:larger;">
+            2026 ·· Llicència: &#x1F16D; &#x1F16F; &#x1F10E; &#x1F10F;
+    </p>
     `;
 
 });
